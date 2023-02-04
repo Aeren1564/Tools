1672f067d625497c1d8271ff62dbe4d5f4789f69
Aeren <58352696+Aeren1564@users.noreply.github.com>
2023-02-04 12:19:32 +0900

    Add files via upload

--- plot.py
+++ plot.py
@@ -8,7 +8,7 @@ updates = {
 }
 
 while True:
-	mode = input("> Type\n> '0' or 'show' to show the plot,\n> '1' or 'point' to add points,\n> '2' or 'polygon' to add polygons,\n> '3' or 'polyline' to add polylines,\n> '4' or 'circle' to add circles,\n> '-1' or 'clear' to empty the plot\n> ")
+	mode = input("> Type\n> '0' or 'show' to show the plot,\n> '1' or 'point' to add points,\n> '2' or 'line' to add lines,\n> '3' or 'polygon' to add polygons,\n> '4' or 'polyline' to add polylines,\n> '5' or 'circle' to add circles,\n> '-1' or 'clear' to empty the plot\n> ")
 
 	if mode == '-1' or mode == 'clear':
 		updates['point']['X'].clear()
@@ -57,34 +57,40 @@ while True:
 	elif mode == '1' or mode == 'point':
 		n = int(input('> # of points: '))
 		for i in range(n):
-			x, y = [int(v) for v in input('> x y: ').split(' ')]
+			x, y = [float(v) for v in input('> x y: ').split(' ')]
 			updates['point']['X'].append(x)
 			updates['point']['Y'].append(y)
 
-	elif mode == '2' or mode == 'polygon':
+	elif mode == '2' or mode == 'line':
+		n = int(input('> # of lines: '))
+		for i in range(n):
+			x0, y0, x1, y1 = [float(v) for v in input('> x0 y0 x1 y1: ').split(' ')]
+			updates['polyline'].append([(x0, y0), (x1, y1)]);
+
+	elif mode == '3' or mode == 'polygon':
 		n = int(input('> # of polygons: '))
 		for i in range(n):
 			m = int(input('> # of vertices: '))
 			p = []
 			for j in range(m):
-				x, y = [int(v) for v in input('> x y: ').split(' ')]
+				x, y = [float(v) for v in input('> x y: ').split(' ')]
 				p.append((x, y))
 			p.append(p[0])
 			updates['polygon'].append(p)
 
-	elif mode == '3' or mode == 'polyline':
+	elif mode == '4' or mode == 'polyline':
 		n = int(input('> # of polylines: '))
 		for i in range(n):
 			m = int(input('> # of vertices: '))
 			p = []
 			for j in range(m):
-				x, y = [int(v) for v in input('> x y: ').split(' ')]
+				x, y = [float(v) for v in input('> x y: ').split(' ')]
 				p.append((x, y))
 			updates['polyline'].append(p)
 
-	elif mode == '4' or mode == 'circle':
+	elif mode == '5' or mode == 'circle':
 		n = int(input('> # of circles: '))
 		for i in range(n):
-			x, y, r = [int(v) for v in input('> x y r: ').split(' ')]
+			x, y, r = [float(v) for v in input('> x y r: ').split(' ')]
 			updates['circle'].append((x, y, r))
 	print('>')
